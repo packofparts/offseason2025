53394a7e37cd3ca95d2f20f6ea70d84b08430a04
Programming1294 <programming-leads@packofparts.org>
2025-12-10 19:39:04 -0800

    Implemented Flywheel (#5)
    * coded flywheel
    
    * changed Flywheel to singleton, made new button binding, made const var
    
    * added periodic override
    
    ---------
    
    Co-authored-by: Programming1294 <programming-leads@packofparts.com>

---
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+import poplib.subsytems.flywheel.TalonFlywheel;
+
+public class Flywheel extends TalonFlywheel {
+    private static Flywheel instance = null;
+
+    public static Flywheel getInstance() {
+        if (instance == null) {
+            instance = new Flywheel();
+        }
+        return instance;
+    }
+
+    private Flywheel() {
+        super(
+            Constants.Flywheel.lead, 
+            Constants.Flywheel.follow, 
+            "Flywheel", 
+            Constants.Flywheel.TUNING_MODE, 
+            false
+        );
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+    }
+}
